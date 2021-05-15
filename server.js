9533170772a3bcb15f3cf0a16c9d9c33c9001e53
Laerte <laerte@mztecnologia.com.br>
2021-05-15 16:41:34 -0300

    Creating first route and listening using ENV file

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require('express')
 const morgan = require('morgan')
 const dotenv = require('dotenv')
 
+// Load env
+dotenv.config({ path: './config.env' })
+
 const app = express()
 
-app.listen(5000, () => {
-    console.log('App listening on port 5000!')
+app.get('/api/v1/profile/:platform/:gamertag', (req, res) => {
+    console.log(req.params.platform, req.params.gamertag)
+    res.send("Hello")
+})
+
+const port = process.env.PORT || 8011
+
+app.listen(port, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}!`)
 })
