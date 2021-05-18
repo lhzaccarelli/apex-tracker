c2d79ae667eab0806546fd377f5274aeb6f8289f
Laerte <laerte@mztecnologia.com.br>
2021-05-18 09:57:13 -0300

    First implementation of get request - gamer profile

--- routes/profile.js
+++ routes/profile.js
@@ -1,10 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const fetch = require('node-fetch')
 
 // /api/v1/profile
-router.get('/:platform/:gamertag', (req, res) => {
-    console.log(req.params.platform, req.params.gamertag)
-    res.send("Hello")
+router.get('/:platform/:gamertag', async (req, res) => {
+    try {
+        const headers = {
+            'TRN-Api-Key': process.env.TRACKER_API_KEY
+        }
+
+        const { platform, gamertag } = req.params
+
+        const response = await fetch(`${process.env.TRACKER_API_URL}/profile/${platform}/${gamertag}`, {
+            headers
+        })
+
+        const data = await response.json()
+
+        res.json(data)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({
+            message: 'Server Error'
+        })
+    }
 })
 
 module.exports = router
