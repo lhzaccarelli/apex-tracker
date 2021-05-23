d87a02e114be0aeab15b1d9b53fe3ef6923d4420
Laerte Zaccarelli <lhzaccarelli@gmail.com>
2021-05-23 13:58:21 -0300

    Search component added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="container" id="app">
     <Header />
+    <Search />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header"
+import Search from "./components/Search"
 
 export default {
   name: 'App',
-  components: { Header }
+  components: { Header, Search }
 }
 </script>
 
